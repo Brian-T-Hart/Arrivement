0394db122b70d94a5d933fac7836ca1ae5f38bc7
Brian-T-Hart <hart.briant@gmail.com>
2019-01-08 16:01:51 -0800

    added select-user-type to register form

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -61,6 +61,17 @@
                             </div>
                         </div>
 
+                        <div class="form-group row">
+                            <label for="type" class="col-md-4 col-form-label text-md-right">Register As</label>
+
+                            <div class="col-md-6">
+                                <select id="select-user-type" class="form-control" name="type" required>
+                                    <option>Buyer</option>
+                                    <option>Seller</option>
+                                </select>
+                            </div>
+                        </div>
+
                         <div class="form-group row mb-0">
                             <div class="col-md-6 offset-md-4">
                                 <button type="submit" class="btn btn-primary">
